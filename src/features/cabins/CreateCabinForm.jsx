f2b1b852bdf8c933c6ddecd6cb72673999706fc1
karadza_b <bayezitkarajaew@gmail.com>
2024-05-19 14:08:20 +0500

    First commit

---
+++ src/features/cabins/CreateCabinForm.jsx
@@ -0,0 +1,165 @@
+import { useForm } from 'react-hook-form';
+
+import Button from '../../ui/Button';
+import FileInput from '../../ui/FileInput';
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Textarea from '../../ui/Textarea';
+import { Input } from './../../ui/Input';
+
+import { useCreateCabin } from './useCreateCabin';
+import { useUpdateCabin } from './useUpdateCabin';
+
+function CreateCabinForm({ cabinToUpdate = {}, closeModal }) {
+  const { id: updateId, ...editValues } = cabinToUpdate;
+  const isEditSession = Boolean(updateId);
+  const { register, handleSubmit, reset, getValues, formState } = useForm({
+    defaultValues: isEditSession ? editValues : {},
+  });
+  const { errors } = formState;
+
+  const { isCreating, createCabin } = useCreateCabin();
+  const { isUpdating, updateCabin } = useUpdateCabin();
+
+  const isWorking = isUpdating || isCreating;
+
+  const onSubmit = (data) => {
+    console.log(data);
+    const image = typeof data.image === 'string' ? data.image : data.image[0];
+    if (isEditSession)
+      updateCabin(
+        { newCabinData: { ...data, image }, id: updateId },
+        {
+          onSuccess: (data) => {
+            reset();
+            closeModal?.();
+          },
+        },
+      );
+    else
+      createCabin(
+        { ...data, image: image },
+        {
+          onSuccess: (data) => {
+            reset();
+            closeModal?.();
+          },
+        },
+      );
+  };
+  const onError = (err) => {
+    console.log(err);
+  };
+
+  return (
+    <Form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      type={closeModal ? 'modal' : 'regular'}
+    >
+      <FormRow
+        label='Cabin name'
+        error={errors?.name?.message}
+      >
+        <Input
+          type='text'
+          id='name'
+          disabled={isWorking}
+          {...register('name', {
+            required: 'this field is required',
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label='Maximum capacity'
+        error={errors?.maxCapacity?.message}
+      >
+        <Input
+          type='number'
+          id='maxCapacity'
+          disabled={isWorking}
+          {...register('maxCapacity', {
+            required: 'This field is required',
+            min: {
+              value: 1,
+              message: 'Capacity should be at least 1',
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label='Regular price'
+        error={errors?.regularPrice?.message}
+      >
+        <Input
+          type='number'
+          id='regularPrice'
+          disabled={isWorking}
+          {...register('regularPrice', {
+            required: 'This field is required',
+            min: {
+              value: 1,
+              message: 'Capacity should be at least 1',
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label='Discount'
+        error={errors?.discount?.message}
+      >
+        <Input
+          type='number'
+          id='discount'
+          defaultValue={0}
+          disabled={isWorking}
+          {...register('discount', {
+            required: 'This field is required',
+            validate: (value) => value <= getValues().regularPrice || 'Discount should be less than regular price',
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label='Description for website'
+        error={errors?.description?.message}
+      >
+        <Textarea
+          type='number'
+          id='description'
+          defaultValue=''
+          disabled={isWorking}
+          {...register('description', {
+            required: 'This field is required',
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Cabin photo'>
+        <FileInput
+          id='image'
+          accept='image/*'
+          {...register('image', {
+            required: isEditSession ? false : 'This file is required',
+          })}
+        />
+      </FormRow>
+
+      <FormRow>
+        {/* type is an HTML attribute! */}
+        <Button
+          variation='secondary'
+          type='reset'
+          onClick={() => closeModal?.()}
+        >
+          Cancel
+        </Button>
+        <Button disabled={isWorking}>{isEditSession ? 'Edit cabin' : 'Create new cabin'}</Button>
+      </FormRow>
+    </Form>
+  );
+}
+
+export default CreateCabinForm;
---
+++ src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+export const supabaseUrl = 'https://kcktbvrmzrlsdxchdcsv.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtja3RidnJtenJsc2R4Y2hkY3N2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMDE1ODI5NSwiZXhwIjoyMDI1NzM0Mjk1fQ.PXkzjJA0VeQYAEeLycGhsCA8iI8Hk_JZr6zWd00KzO8';
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
