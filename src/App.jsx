427258db267fe1c9d619276a9030632bff9d8051
karadza_b <bayezitkarajaew@gmail.com>
2024-05-30 16:24:13 +0500

    add dark-mode

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import AppLayout from "./ui/AppLayout";
 import Booking from "./pages/Booking";
 import Checkin from "./pages/Checkin";
 import ProtectedRoute from "./ui/ProtectedRoute";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,7 +28,7 @@ const queryClient = new QueryClient({
 
 const App = () => {
   return (
-    <>
+    <DarkModeProvider>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
 
@@ -77,7 +78,7 @@ const App = () => {
           }}
         />
       </QueryClientProvider>
-    </>
+    </DarkModeProvider>
   );
 };
 
